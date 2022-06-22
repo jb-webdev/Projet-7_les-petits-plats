24d28d41061f21437a865dc75a65df5ed19353f7
jb.webdev <j.boero@icloud.com>
2022-06-22 22:51:34 +0200

    MEP message error

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ class Index {
 		this.recherche = ''
 		this.eventResearchBar = document.getElementById('inputResearch')
 		this.objetTagRecherche = new Object()
+		this.cardError = document.querySelector('.cardError')
 	}
 	/**
 	 * filtre les recettes selon le choix de l'utilisateur
@@ -18,6 +19,32 @@ class Index {
 	 */
 	displayReceips(datasRecipe) {
 		this.containerRecipeCards.innerHTML = ''
+		/** Fonction pour l'affichage de la carte Error avec redirection */
+		const displayCardError = () => {
+			const cardsError = `
+			<div class="cardError">
+				<p><strong>Error</strong></p>
+				<p>La recette n'existe pas !</p>
+				<p>Ou bien un ou plusieurs critère doit être modifier ou suprimé</p>
+				<p>vous pouvez rechercher par exemple : <strong>COCO</strong></p>
+				<p>le moteur vous retournera les recettes contenant le therm <strong>COCO</strong></p>
+			</div>
+			`
+			this.containerRecipeCards.innerHTML = cardsError
+			setTimeout(() => {
+				this.containerRecipeCards.innerHTML = ''
+				this.eventResearchBar.value = ''
+				datasRecipe
+				.map(recipe => new Recipe(recipe))
+				.forEach(recipe => {
+					const Factories = new Card(recipe)
+					this.containerRecipeCards.appendChild(
+						Factories.createRecipeCard()
+					)
+				})
+			}, 3000)
+			
+		}
 		/** Function Algo 2 */
 		function filterData(term, arrayToLoop) {
 			var filteredReceipes = []
@@ -44,43 +71,55 @@ class Index {
 			// quand la barre de recherche et rempli et que aucun tags n'est selectionner on filtre qu'avec le term de la barre de recherche
 			let resultatRecherche = this.objetTagRecherche.bar
 			var filtreArray  = filterData(resultatRecherche, datasRecipe)
-
-			filtreArray.map(recipe => new Recipe(recipe))
-				.forEach(recipe => {
-					const Factories = new Card(recipe)
-					this.containerRecipeCards.appendChild(
-						Factories.createRecipeCard()
-					)
-				})
+			if (filtreArray.length === 0) {
+				displayCardError()
+			} else {
+				filtreArray.map(recipe => new Recipe(recipe))
+					.forEach(recipe => {
+						const Factories = new Card(recipe)
+						this.containerRecipeCards.appendChild(
+							Factories.createRecipeCard()
+						)
+					})
+			}
 		} else if (this.objetTagRecherche.bar === '' && this.objetTagRecherche.tags.length > 0){
 			// quand la barre de recherche et vide et au moins un tags est selectionné on filtre qu'avec les terms des tags
-			var filterAllTags = datasRecipe
+			var filtreArray = datasRecipe
 			for (let i = 0; i < this.objetTagRecherche.tags.length; i++){
-				filterAllTags = filterData(this.objetTagRecherche.tags[i], filterAllTags)
+				filtreArray = filterData(this.objetTagRecherche.tags[i], filtreArray)
 			}
-			filterAllTags
-				.map(recipe => new Recipe(recipe))
-				.forEach(recipe => {
-					const Factories = new Card(recipe)
-					this.containerRecipeCards.appendChild(
-						Factories.createRecipeCard()
-					)
-				})
+			if (filtreArray.length === 0) {
+				displayCardError()
+			} else {
+				filtreArray
+					.map(recipe => new Recipe(recipe))
+					.forEach(recipe => {
+						const Factories = new Card(recipe)
+						this.containerRecipeCards.appendChild(
+							Factories.createRecipeCard()
+						)
+					})
+			}
 		} else if (this.objetTagRecherche.bar != '' && this.objetTagRecherche.tags.length > 0){
 			// quand la barre de recherche et au moins un tags est selectionné on filtre l'ensemble de la recherche
 			let resultatRecherche = this.objetTagRecherche.bar
-			var AllFilterResearch = filterData(resultatRecherche, datasRecipe)
+			var filtreArray = filterData(resultatRecherche, datasRecipe)
+			
 			for (let i = 0; i < this.objetTagRecherche.tags.length; i++){
-				AllFilterResearch = filterData(this.objetTagRecherche.tags[i], AllFilterResearch)
+				filtreArray = filterData(this.objetTagRecherche.tags[i], filtreArray)
 			}
-			AllFilterResearch
-				.map(recipe => new Recipe(recipe))
-				.forEach(recipe => {
-					const Factories = new Card(recipe)
-					this.containerRecipeCards.appendChild(
-						Factories.createRecipeCard()
-					)
-				})
+			if (filtreArray.length === 0) {
+				displayCardError()
+			} else {
+				filtreArray
+					.map(recipe => new Recipe(recipe))
+					.forEach(recipe => {
+						const Factories = new Card(recipe)
+						this.containerRecipeCards.appendChild(
+							Factories.createRecipeCard()
+						)
+					})
+			}
 		}
 	}
 	async main() {
