2fe5ceb9a7368fd39b397e4416ff7a57f509ec53
jb.webdev <j.boero@icloud.com>
2022-06-25 22:18:35 +0200

    MEP de la methode pour l'affichage des recettes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 import { ReceiptsProvider } from './provider/provider.js'
 
+import { Card } from './Factories/Card.js'
+import { Recipe } from './models/Recipe.js'
 
 
 class Index {
 	constructor() {
 		this.receiptsProvider = new ReceiptsProvider('../data/recipes.json')
+		this.containerRecipeCards = document.querySelector('.wrapperCards')
+		this.recherche = new Object
 		
 	}
-	
+	/**Methode pour l'affichage des recettes */
+	displayReceips(arrayRecherche) {
+		arrayRecherche
+			.map(recipe => new Recipe(recipe))
+			.forEach(recipe => {
+				const Factories = new Card(recipe)
+				this.containerRecipeCards.appendChild(
+					Factories.createRecipeCard()
+				)
+			})
+	}
 	
 	async main() {
 		const recipeData = await this.receiptsProvider.getDataReceipts()
-		
+		this.displayReceips(recipeData)
 		
 	}
 }
